9580968c34630dd9753f9699855813b58123a994
VageX <wfgcarty@gmail.com>
2019-03-31 21:32:17 -0500

    Added Chapter 3 Added bicycles.py Added bicycles1.py Added bicycles2.py Added cars.py Added cars1.py Added cars2.py Added motorcycles.py Added motorcycles1.py Added motorcycles2.py Added motorcycles3.py Added motorcycles4.py Added motorcycles5.py Added motorcycles6.py Completed Chapter 3

---
+++ Chapter 3/motorcycles2.py
@@ -0,0 +1,10 @@
+motorcycles = []
+
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
