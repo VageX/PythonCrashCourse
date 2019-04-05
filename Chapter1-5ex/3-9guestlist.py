7c0c48e050e6c00fd6cc6ff1da4c69a83a976b06
VageX <wfgcarty@gmail.com>
2019-04-04 23:59:25 -0500

    Added 3-8locations.py Added 3-9guestlist.py

---
+++ Chapter1-5ex/3-9guestlist.py
@@ -0,0 +1,5 @@
+guest_list = ['Lauren', 'Biscuit', 'Myself']
+print(guest_list[0] + ", you are invited to dinner.")
+print(guest_list[1] + ", you are invited to dinner.")
+print(guest_list[2] + ", you are invited to dinner.")
+print(len(guest_list))
