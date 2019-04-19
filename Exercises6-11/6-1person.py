33beb026d8ff628708d9af7b1e05529df4d358e1
VageX <wfgcarty@gmail.com>
2019-04-18 22:42:21 -0500

    Renamed Exercises1-5 Created Exercises6-11 Added 6-1person.py Added 6-2favorite_numbers.py

---
+++ Exercises6-11/6-1person.py
@@ -0,0 +1,7 @@
+people = {
+    'first_name': 'lauren',
+    'last_name': 'yeung',
+    'age': '29',
+    'city': 'dallas'}
+
+print(people)
---
+++ Exercises6-11/6-2favorite_numbers.py
@@ -0,0 +1,9 @@
+favorite_numbers = {
+    'george': '27',
+    'lauren': '1',
+    'biscuit': '17',
+    'denis': '22',
+    'jasmine': '21'
+    }
+
+print(favorite_numbers)
