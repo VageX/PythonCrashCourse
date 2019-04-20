f96a4d85e6ac96e238c0f747e9ad0f8cb983f41d
VageX <wfgcarty@gmail.com>
2019-04-20 15:32:49 -0500

    Added 8-1message.py Added 8-2favoritebook.py

---
+++ Exercises6-11/Chapter 8/8-1message.py
@@ -0,0 +1,4 @@
+def display_message():
+    """Prints one sentence telling everyone what I am learning about this chapter."""
+
+    print("In this chapter, I am learning about functions!")
---
+++ Exercises6-11/Chapter 8/8-2favoritebook.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    """Prints a message about my favorite books."""
+    print("One of my favorite books is " + title + ".")
+
+
+favorite_book('12 Rules for Life: An Antidote to Chaos')
