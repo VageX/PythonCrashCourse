f785a6fe64cc3e13069faea02e3f1be2c82b0028
VageX <wfgcarty@gmail.com>
2019-04-22 23:08:47 -0500

    Added 9-4numberserved.py Added 9-5loginattempts.py

---
+++ Exercises6-11/Chapter 9/9-5loginattempts.py
@@ -0,0 +1,38 @@
+"""Make a class named 'User' with several attributes"""
+
+class User():
+
+    def __init__(self, first_name, last_name, age, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.gender = gender
+        self.full_name = str.title(self.first_name) + " " + str.title(self.last_name)
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print("\nFirst Name: " + str.title(self.first_name))
+        print("Last Name: " + str.title(self.last_name))
+        print("Age: " + str(self.age))
+        print("Gender: " + str.title(self.gender))
+
+    def greet_user(self):
+        print("\nWelcome " + str.title(self.full_name) + " to the program!")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        print("\n" + str(self.full_name) + " has logged in " + str(self.login_attempts) + " many times.")
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print("\n" + str(self.full_name) + " has logged in " + str(self.login_attempts) + " many times.")
+
+person_one = User('george', 'carty', '31', 'male')
+person_one.describe_user()
+person_one.greet_user()
+person_one.increment_login_attempts()
+person_one.increment_login_attempts()
+person_one.increment_login_attempts()
+
+person_one.reset_login_attempts()
+person_one.increment_login_attempts()
